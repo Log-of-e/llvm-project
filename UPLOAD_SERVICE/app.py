4019ba0c1f02ccc2987e4c6df135361151d6748f
Nesim <nesim.engineer@gmail.com>
2020-10-30 03:13:13 -0700

    when receiving post of ll file:run opt, save output in json format

--- UPLOAD_SERVICE/app.py
+++ UPLOAD_SERVICE/app.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, request, abort, jsonify
+import subprocess
+import shlex
+import json
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
@@ -26,10 +29,10 @@ def hello():
     return htmlstring
 
 
-@app.route('/upload',methods = ['POST'])
-def upload():
+@app.route('/upload0',methods = ['POST'])
+def upload0():
     data = "ll file saved"
-    if request.method == 'POST': # Checks if it's a POST request
+    if request.method == 'POST':
         uploaded_file = request.files['upload']
         llname=request.form['llname']
         file_ext = os.path.splitext(llname)[1]
@@ -38,6 +41,48 @@ def upload():
         response = jsonify(data) 
         response.status_code = 202 
         return response 
+
+@app.route('/upload',methods = ['POST'])
+def upload():
+    data = "ll file saved"
+    if request.method == 'POST':
+        uploaded_file = request.files['upload']
+        llname=request.form['llname']
+        file_ext = os.path.splitext(llname)[1]
+        if uploaded_file.filename != '':
+            __filename=os.path.join(app.config['UPLOAD_PATH'], llname)
+            uploaded_file.save(__filename)
+            saveOptRun(__filename)
+        response = jsonify(data) 
+        response.status_code = 202 
+        return response 
+
+
+def saveOptRun(filename):
+    agiantstr =  "../build/bin/opt  -disable-output  ./{0} -passes=helloworld".format(filename)
+    result = subprocess.run( shlex.split(agiantstr) , capture_output=True, text=True)
+    infostr = result.stdout
+    infoList = splitOptOutput(infostr)
+    with open("./"+ filename+".json", "w") as outfile:
+        json.dump(infoList, outfile)
+    return
+
+def splitOptOutput(_infostr):
+    resultList=[]
+    a1=_infostr.split("End HelloWorldPass\n" )
+    a2=  [i for i in a1 if i] 
+    for a in a2:
+        methodDictionary={}
+        splitArr0 = a.split("\n")[1:]
+        methodDictionary['function']=splitArr0[0].split(":")[1].strip()
+        methodDictionary['rettype']=splitArr0[1].split(":")[1].strip()
+        methodDictionary['cconv']=splitArr0[2].split(":")[1].strip()
+        methodDictionary['isns']=splitArr0[3].split(":")[1].strip()
+        resultList.append(methodDictionary)
+    # print("resultList is {0}".format(resultList))
+    return resultList
+
+
 
 if __name__ == '__main__':
     app.run()
