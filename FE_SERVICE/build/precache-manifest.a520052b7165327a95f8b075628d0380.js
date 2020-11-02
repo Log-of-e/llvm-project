2efa6d7ab24ae1a104bbdc450b23f13a3db92a57
Nesim <nesim.engineer@gmail.com>
2020-11-01 19:44:34 -0800

    static build of FE

---
+++ FE_SERVICE/build/precache-manifest.a520052b7165327a95f8b075628d0380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5f9ccebf5a9c9ce6b93641b8282880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a25663ed94847ff3934",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "26f144558a31716a3678",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "5a25663ed94847ff3934",
+    "url": "/static/js/2.2e6ab793.chunk.js"
+  },
+  {
+    "revision": "8da9ff4ecc7440c09f64cd8625501c41",
+    "url": "/static/js/2.2e6ab793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f144558a31716a3678",
+    "url": "/static/js/main.36d39444.chunk.js"
+  },
+  {
+    "revision": "95bfb3c06fdfaa27a10f",
+    "url": "/static/js/runtime-main.f01611f9.js"
+  }
+]);
